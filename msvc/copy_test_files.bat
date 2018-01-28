39b48477085f0e4aeaccbeb3bb8ad8b9d979cd15
Antoine <end2endzone@gmail.com>
2018-01-28 17:42:24 -0500

    * Fixed compilation issues. * Modified copy_test_files.bat script to copy test files to Release directory for MSVC builds. * Modified unit tests to call copy_test_files.bat script as post build command.

--- msvc/copy_test_files.bat
+++ msvc/copy_test_files.bat
@@ -1,3 +1,16 @@
 @echo off
+
+echo =======================================================================
+echo Copying test files for unit tests...
+echo =======================================================================
+
+REM Copy test files to project folder to be able to run tests from Visual Studio IDE
+cd /d %~dp0
 mkdir test_files >NUL 2>NUL
 copy /v "..\tests\test_files\*.*" "%~dp0test_files"
+
+
+REM Copy test files to outdir for NSIS
+cd /d %~dp0
+mkdir Win32\Release\test_files >NUL 2>NUL
+copy /v "..\tests\test_files\*.*" "%~dp0Win32\Release\test_files"
